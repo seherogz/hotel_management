2ba1516399d9021e13eb529c61ccf069d1d564e2
Seher Oğuz <20210808004@ogr.akdeniz.edu.tr>
2025-04-20 16:48:59 +0300

    frontend mobil eklendi

--- Frontend/src/services/apiService.js
+++ Frontend/src/services/apiService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Set the base URL for API requests
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api/v1';
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
 
 // Create an axios instance with default config
 const apiClient = axios.create({
